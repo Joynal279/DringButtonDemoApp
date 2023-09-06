dcb09912001d61877a3ed8cfee007df96e1e6353
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-06 16:37:46 +0600

    Initial commit

---
+++ DringButtonDemoApp Watch App/DringButtonDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DringButtonDemoAppApp.swift
+//  DringButtonDemoApp Watch App
+//
+//  Created by Joynal Abedin on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct DringButtonDemoApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
