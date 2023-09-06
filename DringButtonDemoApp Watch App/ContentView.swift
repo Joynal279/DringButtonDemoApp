8e3722d7bbf430d0f71e014455ba10ad52f4e6a8
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-06 18:13:21 +0600

    Button UI implemented with animation

--- DringButtonDemoApp Watch App/ContentView.swift
+++ DringButtonDemoApp Watch App/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isPlaying: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            Circle()
+                .stroke(Color.red, lineWidth: 4.0)
+                .frame(width: 130, height: 130, alignment: .center)
+                .scaleEffect(self.isPlaying ? 1.3 : 1.1)
+                .opacity(self.isPlaying ? 0 : 1)
+                .animation(.easeInOut(duration: 1), value: isPlaying)
+            
+            Button {
+                isPlaying.toggle()
+            } label: {
+                Text("Dring Dring")
+                    .italic()
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 130, height: 130)
+                    .foregroundStyle(.foreground)
+            }
+            .buttonStyle(PlainButtonStyle())
+            .background(.blue)
+            .clipShape(Circle())
         }
-        .padding()
+
     }
 }
 
